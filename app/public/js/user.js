c5c72b51d1c1b8ab4e920858dadcd97d6e5a223f
Shivali Pathak <42498941+shivalipathak@users.noreply.github.com>
2021-09-21 16:41:09 -0400

    Create user.js
    javascript for random user

---
+++ app/public/js/user.js
@@ -0,0 +1,49 @@
+const UserInfo = {
+    data() {
+      return {
+        "person": {},
+        "offers": [
+                {
+                    "id": 1,
+                    "name": "Janet Doe",
+                    "salary": 120000,
+                    "bonus": 9000,
+                    "company":"EY",
+                    "offerDate": "2021-09-08"
+                },
+                {
+                    "id": 2,
+                    "name": "Jordan Doe",
+                    "salary": 80000,
+                    "bonus": 2000,
+                    "company":"IU",
+                    "offerDate": "2021-08-09"
+                }
+            ]
+        }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.person = responseJson.results[0];
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        }
+    },
+    created() {
+        this.fetchUserData();
+    }
+}
+  
+Vue.createApp(UserInfo).mount('#appRandom');
